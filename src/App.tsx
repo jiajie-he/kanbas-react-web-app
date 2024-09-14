7d7400d2a1c4d139d88362bc62e3263dcc9c241a
Jiajie He <jiajiehe@yahoo.com>
2024-09-13 21:29:34 -0700

    update toc

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Labs from './Labs';
 import Kanbas from "./Kanbas";
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Link, Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
       <div>
           <h1>Welcome to Web Dev</h1>
+          <Link to="/Labs">Labs</Link> | <Link to="/Kanbas">Kanbas</Link>
           <Routes>
             <Route path="/" element={<Navigate to="Labs" />} />
             <Route path="/Labs/*" element={<Labs />} />
