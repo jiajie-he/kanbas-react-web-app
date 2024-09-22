e5159faed7aa6798050801f0b3afbd6aedb516ee
Jiajie He <jiajiehe@yahoo.com>
2024-09-21 18:10:27 -0700

    completed section 3

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,6 @@ function App() {
   return (
     <HashRouter>
       <div>
-          <h1>Welcome to Web Dev</h1>
-          <h2>Jiajie He - SEC 01</h2>
-          <a id="wd-github" href="https://github.com/jannunzi/kanbas-react-web-app-cs5610-fa24">click here </a>
-          to go to relevant source code repositories
-          {/* <Link to="/Labs">Labs</Link> | <Link to="/Kanbas">Kanbas</Link> */}
           <Routes>
             <Route path="/" element={<Navigate to="Labs" />} />
             <Route path="/Labs/*" element={<Labs />} />
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,10 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
+      <h1>Welcome to Web Dev</h1>
+      <h2>Jiajie He - SEC 01</h2>
+      <a id="wd-github" href="https://github.com/jannunzi/kanbas-react-web-app-cs5610-fa24">click here </a>
+      to go to relevant source code repositories
       <h1>Labs</h1>
       <TOC />
       <Routes>
