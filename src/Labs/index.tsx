a8e8e8f134490818f40a719589430fd02727006e
Jiajie He <jiajiehe@yahoo.com>
2024-09-29 11:50:38 -0700

    adding container

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,10 +5,10 @@ import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 export default function Labs() {
   return (
-    <div>
+    <div className="container-fluid">
       <h1>Welcome to Web Dev</h1>
       <h2>Jiajie He - SEC 01</h2>
-      <a id="wd-github" href="https://github.com/jannunzi/kanbas-react-web-app-cs5610-fa24">click here </a>
+      <a id="wd-github" target="_blank" href="https://github.com/jannunzi/kanbas-react-web-app-cs5610-fa24">click here </a>
       to go to relevant source code repositories
       <h1>Labs</h1>
       <TOC />
