7b7f88f5a66e0611cd8f28cd9964970bd6cc1a44
Jiajie He <jiajiehe@yahoo.com>
2024-09-30 21:20:26 -0700

    account screen

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -5,6 +5,7 @@ import Home from "./Home";
 import Modules from "./Modules";
 import { Navigate, Route, Routes } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
+import PeopleTable from "./People/Table";
 
 export default function Courses() {
   return (
@@ -23,7 +24,7 @@ export default function Courses() {
             <Route path="Modules" element={<Modules />} />
             <Route path="Assignments" element={<Assignments />} />
             <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-            <Route path="People" element={<h2>People</h2>} />
+            <Route path="People" element={<PeopleTable />} />
           </Routes>
         </div>
       </div>
