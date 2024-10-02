faebf08bb11dfe5b82e58b2ef3ff405e1cbed999
Jiajie He <jiajiehe@yahoo.com>
2024-10-01 17:11:52 -0700

    updating landing page

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -2,12 +2,16 @@ import { Link } from "react-router-dom";
 export default function Profile() {
   return (
     <div id="wd-profile-screen">
+      <h1>Welcome to Web Dev</h1>
+      <h2>Jiajie He - SEC 01</h2>
+      <a id="wd-github" target="_blank" href="https://github.com/jannunzi/kanbas-react-web-app-cs5610-fa24">click here </a>
+      to go to relevant source code repositories
       <h1>Profile</h1>
-      <input id="wd-username" value="alice" placeholder="username" className="form-control mb-2"/>
+      <input id="wd-username" value="Jiajie He" placeholder="username" className="form-control mb-2"/>
       <input id="wd-password" value="123" placeholder="password"
              type="password" className="form-control mb-2"/>
-      <input id="wd-firstname" value="Alice" placeholder="First Name" className="form-control mb-2"/>
-      <input id="wd-lastname" value="Wonderland" placeholder="Last Name" className="form-control mb-2"/>
+      <input id="wd-firstname" value="Jiajie" placeholder="First Name" className="form-control mb-2"/>
+      <input id="wd-lastname" value="He" placeholder="Last Name" className="form-control mb-2"/>
       <input id="wd-dob" value="2000-01-01" type="date" className="form-control mb-2"/>
       <input id="wd-email" value="alice@wonderland" type="email" className="form-control mb-2"/>
       <select id="wd-role" className="form-control mb-2">
--- src/Kanbas/Courses/Modules/GreenCheckmark.tsx
+++ src/Kanbas/Courses/Modules/GreenCheckmark.tsx
@@ -4,6 +4,6 @@ export default function GreenCheckmark() {
     <span className="me-1 position-relative">
       <FaCheckCircle style={{ top: "2px" }}
         className="text-success me-1 position-absolute fs-5" />
-      <FaCircle className="text-white me-1 fs-6" />
+      <FaCircle className="text-white me-2 fs-6" />
     </span>
 );}
