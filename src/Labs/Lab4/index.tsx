3905d9023160248c0fc99b63d6e2d4203199db98
Jiajie He <jiajiehe@yahoo.com>
2024-10-21 12:35:49 -0700

    initalize a4

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,27 @@
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+export default function Lab1() {
+    function sayHello() {
+        alert("Hello");
+      }
+    return (
+        <div id="wd-lab4">
+            <h2>Lab 4</h2>
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />   
+        </div>
+    );
+}
+  
