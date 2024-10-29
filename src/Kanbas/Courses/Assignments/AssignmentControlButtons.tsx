aecdd18cfea38225d99759fe62c9ffe95b05379a
Jiajie He <jiajiehe@yahoo.com>
2024-10-28 20:59:42 -0700

    a4 Redux

--- src/Kanbas/Courses/Assignments/AssignmentControlButtons.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentControlButtons.tsx
@@ -2,11 +2,16 @@ import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
 import { FaTrash } from "react-icons/fa";
 
-export default function AssignmentControlButtons() {
+export default function AssignmentControlButtons({ deleteAssignment, assignmentId }: { deleteAssignment: (assignmentId: any) => void; assignmentId: any }) {
+  const handleDelete = () => {
+    deleteAssignment(assignmentId);
+  };
+
   return (
     <div className="float-end">
-      <FaTrash className="text-danger me-2 mb-1" />
+      <FaTrash className="text-danger me-2 mb-1" onClick={handleDelete} />
       <GreenCheckmark />
       <IoEllipsisVertical className="fs-4" />
     </div>
-);}
+  );
+}
